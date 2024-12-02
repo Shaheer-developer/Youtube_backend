6e495cf603f001ffa437c5561798e68d92b666a2
Shaheer-developer <m.shaheer062006@gmail.com>
2024-12-02 23:01:29 +0500

    added endpoints to update password , details , avatar and cover image and endpoints to get userchannel profile and watch history

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { registerUser , login_user, logoutuser , refreshaccesstoken } from "../controllers/user.controller.js";
+import { registerUser , login_user, logoutuser , refreshaccesstoken, changeCurrentPassword, getCurrentuser, changecurrentdetails, updateavatar, updatecover, getuserchannelprofile, getwatchhistory} from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
 
@@ -27,4 +27,19 @@ router.route("/login").post(login_user)
 router.route("/logout").post(verifyJwt, logoutuser)
 
 router.route("/refreshtoken").post(refreshaccesstoken)
+
+router.route("/change-password").post(verifyJwt, changeCurrentPassword)
+
+router.route("/current-user").get(verifyJwt, getCurrentuser)
+
+router.route("/update-account-datils").patch(verifyJwt,changecurrentdetails)
+
+router.route("/avatar").patch(verifyJwt, upload.single("avatar") , updateavatar)
+
+router.route("/cover-image").patch(verifyJwt, upload.single("/coverImage") , updatecover)
+
+router.route("/c/:usernmae").get(verifyJwt, getuserchannelprofile)
+
+router.route("/history").get(verifyJwt, getwatchhistory)
+
 export default router
