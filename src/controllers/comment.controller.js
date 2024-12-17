161df9ade31128c81213b2b3b4cf476140312f5b
Shaheer-developer <m.shaheer062006@gmail.com>
2024-12-17 22:32:54 +0500

    fixed some errors

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -5,7 +5,7 @@ import {Apiresponse} from "../utils/Apiresponse.js"
 import asynchandler from "../utils/asynchandler.js"
 import { Video } from "../models/video.model.js";
 
-const getVideoComments = asyncHandler(async (req, res) => {
+const getVideoComments = asynchandler(async (req, res) => {
     //TODO: get all comments for a video
     const {videoId} = req.params
     const {page = 1, limit = 10} = req.query
@@ -77,7 +77,7 @@ return res.status(200).json(
 
 })
 
-const addComment = asyncHandler(async (req, res) => {
+const addComment = asynchandler(async (req, res) => {
     // TODO: add a comment to a video
     const {videoId} = req.params
     const {content} = req.body
@@ -105,7 +105,7 @@ const addComment = asyncHandler(async (req, res) => {
     )
 })
 
-const updateComment = asyncHandler(async (req, res) => {
+const updateComment = asynchandler(async (req, res) => {
     // TODO: update a comment
    const {commentId} = req.params
    const {content} = req.body
@@ -142,7 +142,7 @@ const updateComment = asyncHandler(async (req, res) => {
 
 })
 
-const deleteComment = asyncHandler(async (req, res) => {
+const deleteComment = asynchandler(async (req, res) => {
     // TODO: delete a comment
     const {commentId} = req.params
     if(!commentId){
